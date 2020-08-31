3b7eb217471f19cb5580dacd670d5a3e87b7dda0
Johanna Jansson <joja92@hotmail.com>
2020-08-31 14:59:52 +0200

    Added products data

--- src/store/index.js
+++ src/store/index.js
@@ -5,6 +5,40 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    products: [
+      {
+        name: '"Mister Parka Jr." parka',
+        img: ""
+      },
+      {
+        name: '"Mister Parke Jr." parka'
+      },
+      {
+        name: 'Sporty T-shirt',
+        img: ''
+      },
+      {
+        name: 'Sporty parka',
+        img: ''
+      },
+      {
+        name: 'Bug stick parka',
+        img: ''
+      },
+      {
+        name: 'Bug stick T-shirt',
+        img: ''
+      },
+      {
+        name: 'Rick roll T-shirt',
+        img: ''
+      },
+      {
+        name: 'Rick roll parka',
+        img: ''
+      }
+    ],
+    cart: []
   },
   mutations: {
   },
