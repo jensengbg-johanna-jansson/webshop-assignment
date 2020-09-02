c425ff7a4784a95a28a69485ed03609e095f5b17
Johanna Jansson <joja92@hotmail.com>
2020-09-02 12:46:21 +0200

    Added tests for emit event

--- tests/unit/sizePicker.spec.js
+++ tests/unit/sizePicker.spec.js
@@ -57,3 +57,39 @@ it('should set value of "selectedSize" to value of first radiobutton when first 
 
     expect(actual).toBe(expected);
 })
+
+it('should emit value of selectedSize every time it is changed', async () => {
+    const expectedEmitsCount = 3;
+    const expected = expectedEmitsCount;
+
+    let wrapper = shallowMount(sizePicker);
+    let radioLabelsArray = wrapper.findAll('label');
+    
+    await radioLabelsArray.at(0).trigger('click');
+    await radioLabelsArray.at(2).trigger('click');
+    await radioLabelsArray.at(1).trigger('click');
+
+    let actual = wrapper.emitted('emitSize').length;
+
+    expect(actual).toBe(expected);
+})
+
+it('last emit should have value of latest clicked label', async () => {
+    let wrapper = shallowMount(sizePicker);
+    let radioButtonsArray = wrapper.findAll('input[type="radio"]');
+
+    const lastClickedItem = 1;
+    const expected = radioButtonsArray.at(lastClickedItem).attributes('value');
+
+    let radioLabelsArray = wrapper.findAll('label');
+
+    await radioLabelsArray.at(0).trigger('click');
+    await radioLabelsArray.at(2).trigger('click');
+    await radioLabelsArray.at(lastClickedItem).trigger('click');
+
+    await wrapper.vm.$nextTick();
+
+    let actual = wrapper.emitted('emitSize')[-1];
+
+    expect(actual).toBe(expected);
+})
