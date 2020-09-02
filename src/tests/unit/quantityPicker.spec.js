49c2971fd811d65d27e98ffecada6b0988e22eb4
Johanna Jansson <joja92@hotmail.com>
2020-09-02 14:53:30 +0200

    Created tests

--- src/tests/unit/quantityPicker.spec.js
+++ src/tests/unit/quantityPicker.spec.js
@@ -1,17 +1,110 @@
 import { shallowMount } from '@vue/test-utils';
-import quantityPicker from '@/components/sizePicker.vue';
+import quantityPicker from '@/components/quantityPicker.vue';
 
 it('should increase number by one when clicking "+"-button', async () => {
+    const expected = 1;
+
+    let wrapper = shallowMount(quantityPicker);
+    let button = wrapper.find('.increase-button');
+    let numberContainer = wrapper.find('p');
+
+    await button.trigger('click');
+
+    let actual = Number(numberContainer.text());
+
+    expect(actual).toBe(expected);
 })
 
 it('should decrease number by one when clicking "-"-button', async () => {
+    const expected = 2;
 
+    let wrapper = shallowMount(quantityPicker);
+    let incButton = wrapper.find('.increase-button');
+    let decButton = wrapper.find('.decrease-button');
+    let numberContainer = wrapper.find('p');
+
+    await incButton.trigger('click');
+    await incButton.trigger('click');
+    await decButton.trigger('click');
+
+    let actual = Number(numberContainer.text());
+
+    expect(actual).toBe(expected);
 })
 
 it('when clicking the "-"-button the number should not go lower than "1"', async () => {
+    const expected = 1;
 
+    let wrapper = shallowMount(quantityPicker);
+    let button = wrapper.find('.decrease-button');
+    let numberContainer = wrapper.find('p');
+
+    await button.trigger('click');
+
+    let actual = Number(numberContainer.text());
+
+    expect(actual).toBe(expected);
 })
 
 it('should emit the current number every time it changes', async () => {
+    const expectedEmitsCount = 3;
+    const expected = expectedEmitsCount;
+
+    let wrapper = shallowMount(quantityPicker);
+    let incButton = wrapper.find('.increase-button');
+    let decButton = wrapper.find('.decrease-button');
     
+    await incButton.trigger('click');
+    await decButton.trigger('click');
+    await incButton.trigger('click');
+
+    let actual = wrapper.emitted('emitSize').length;
+
+    expect(actual).toBe(expected);
+})
+
+it('last emit should have value of the current quantity', async () => {
+    let wrapper = shallowMount(quantityPicker);
+    let incButton = wrapper.find('.increase-button');
+    let decButton = wrapper.find('.decrease-button');
+
+    const expected = 3;
+
+    await incButton.trigger('click');
+    await incButton.trigger('click');
+    await incButton.trigger('click');
+    await decButton.trigger('click');
+
+    await wrapper.vm.$nextTick();
+
+    let actual = wrapper.emitted('emitQuantoty')[3];
+
+    expect(actual).toEqual(expected);
+})
+
+it('should display number "1" on load', async () => {
+    const expected = 1;
+    
+    let wrapper = shallowMount(quantityPicker);
+    let numberContainer = wrapper.find('p');
+
+    let actual = Number(numberContainer.text());
+
+    expect(actual).toBe(expected);
+})
+
+it('if given a quantity prop on load it should display the prop data as the number', async () => {
+    let wrapper = shallowMount(quantityPicker);
+    let expectedQuantity = 3;
+    
+    await wrapper.setProps({ quantity: expectedQuantity })
+
+    const expected = expectedQuantity;
+    
+    let numberContainer = wrapper.find('p');
+
+    let actual = Number(numberContainer.text());
+
+    expect(actual).toBe(expected);
+
 })
