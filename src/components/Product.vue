60430a6c87455c8f3145c1e88c0f93a329e40f54
jensen-ali-suliman <alxso@LAPTOP-92UALMD9.(none)>
2020-09-02 15:22:02 +0200

    Create Product.vue

---
+++ src/components/Product.vue
@@ -0,0 +1,38 @@
+<template>
+  <section class="product">
+    <section class="imgCont"></section>
+    <section class="infoCont">
+      <h3>{{ product.name }}</h3>
+      <p>{{ product.price }} SEK</p>
+      <p class="size">{{ product.size }}</p>
+      <button @click="deleteFunc(product)">delete</button>
+    </section>
+  </section>
+</template>
+
+<script>
+export default {
+  data: () => {
+    return {
+      product: {
+        name: 'First test product',
+        size: 'M',
+        price: 299,
+        img: '',
+      },
+      productToDelete: {},
+    }
+  },
+  methods: {
+    deleteFunc: function(prod) {
+      console.log(prod)
+      this.productToDelete = prod
+      console.log('This is the deleted product', this.productToDelete)
+    },
+  },
+}
+</script>
+
+<style></style>
+
+/* name: 'First test product', size: 'M', price: 299, img: '', */
