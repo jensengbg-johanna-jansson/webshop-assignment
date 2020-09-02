0e410673d958e61e5fff480f186b167c8f64bed5
jensen-ali-suliman <alxso@LAPTOP-92UALMD9.(none)>
2020-09-02 15:22:29 +0200

    Create ShoppingCart.vue

---
+++ src/views/ShoppingCart.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="wrapper">
+    <h1>Shopping cart</h1>
+    <section class="productSection">
+      <Product />
+    </section>
+  </div>
+</template>
+
+<script>
+import Product from '@/components/Product.vue'
+export default {
+  components: {
+    Product,
+  },
+}
+</script>
+
+<style></style>
