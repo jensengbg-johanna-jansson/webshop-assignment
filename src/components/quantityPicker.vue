72537f9b9ee81e47c8c8a7449a8c8f1ca1fd248a
Johanna Jansson <joja92@hotmail.com>
2020-09-02 13:56:57 +0200

    set up base tests

---
+++ src/components/quantityPicker.vue
@@ -0,0 +1,13 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+    name: 'quantityPicker'
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/tests/unit/quantityPicker.spec.js
@@ -0,0 +1,17 @@
+import { shallowMount } from '@vue/test-utils';
+import quantityPicker from '@/components/sizePicker.vue';
+
+it('should increase number by one when clicking "+"-button', async () => {
+})
+
+it('should decrease number by one when clicking "-"-button', async () => {
+
+})
+
+it('when clicking the "-"-button the number should not go lower than "1"', async () => {
+
+})
+
+it('should emit the current number every time it changes', async () => {
+    
+})
