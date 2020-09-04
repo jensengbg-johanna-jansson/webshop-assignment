107e71eb12689cdd1bbd586595a3f61aa4447ece
Albin Sander <albinsander17@gmail.com>
2020-09-04 13:49:21 +0200

    Added tests for product names and images

--- src/Components/ProductPage.vue
+++ src/Components/ProductPage.vue
@@ -2,6 +2,7 @@
 <div class="product">
     <h1>Product Page</h1>
     <h2>Product name</h2>
+    <img src="https://images.unsplash.com/photo-1512436991641-6745cdb1723f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80">
 </div>
     
 </template>
--- tests/unit/ProductPage.spec.js
+++ tests/unit/ProductPage.spec.js
@@ -25,3 +25,15 @@ it("should show the products names", () => {
     expect(content).toBe(expectedName)
 
 })
+
+it("should show the products images", () => {
+    const expectedImage = "https://images.unsplash.com/photo-1512436991641-6745cdb1723f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80";
+    const wrapper = shallowMount(ProductPage);
+    let image = wrapper.find("img src")
+    let content = image.text();
+    
+    expect(content).toBe(expectedImage)
+
+})
+
+
