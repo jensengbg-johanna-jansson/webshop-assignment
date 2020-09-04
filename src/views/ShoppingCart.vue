e9d6474f1f64e5052ae0b2af5e98e15c9f547f30
jensen-ali-suliman <alxso@LAPTOP-92UALMD9.(none)>
2020-09-04 21:43:19 +0200

    Update ShoppingCart.vue

--- src/views/ShoppingCart.vue
+++ src/views/ShoppingCart.vue
@@ -16,4 +16,17 @@ export default {
 }
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.wrapper {
+  width: 100vw;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+
+  h1 {
+    margin: 1rem 0rem 4rem 0rem;
+  }
+}
+</style>
