17ec29accdd99dc1a4e420d75ad415a897e40d62
jensen-ali-suliman <alxso@LAPTOP-92UALMD9.(none)>
2020-09-04 21:43:10 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -9,20 +9,12 @@
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  color: #222;
 }
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+* {
+  padding: 0rem;
+  margin: 0rem;
+  box-sizing: border-box;
 }
 </style>
