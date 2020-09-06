d4fc7733b1a0356612af05ed5d4a3bef39ef32db
jensen-ali-suliman <alxso@LAPTOP-92UALMD9.(none)>
2020-09-06 19:30:36 +0200

    Update delete.spec.js

--- tests/unit/delete.spec.js
+++ tests/unit/delete.spec.js
@@ -2,7 +2,7 @@ import { shallowMount, createLocalVue } from '@vue/test-utils'
 import Product from '@/components/Product.vue'
 
 describe('Delete product', () => {
-  it('Should return the objects when clicked', async () => {
+  it('Should return the clicked object ', async () => {
     const wrapper = shallowMount(Product)
     const expected = wrapper.vm.product
     const deleteBtn = wrapper.find('button')
