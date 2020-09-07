cb6a16d450d4fd2fa377765245e94509758f9e19
Johanna Jansson <joja92@hotmail.com>
2020-09-07 14:09:46 +0200

    Added products data

--- src/store/index.js
+++ src/store/index.js
@@ -5,6 +5,56 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    products: [
+      {
+        id: '1',
+        name: 'A mock parka',
+        price: 299,
+        img: "https://picsum.photos/id/237/1080/720"
+      },
+      {
+        id: '2',
+        name: 'A mock T-shirt',
+        price: 199,
+        img: 'https://picsum.photos/id/237/1080/720'
+      },
+      {
+        id: '3',
+        name: 'Sporty T-shirt',
+        price: 199,
+        img: 'https://picsum.photos/id/237/1080/720'
+      },
+      {
+        id: '4',
+        name: 'Sporty parka',
+        price: 299,
+        img: 'https://picsum.photos/id/237/1080/720'
+      },
+      {
+        id: '5',
+        name: 'Bug stick parka',
+        price: 299,
+        img: 'https://picsum.photos/id/237/1080/720'
+      },
+      {
+        id: '6',
+        name: 'Bug stick T-shirt',
+        price: 199,
+        img: 'https://picsum.photos/id/237/1080/720'
+      },
+      {
+        id: '7',
+        name: 'Rick roll T-shirt',
+        price: 199,
+        img: 'https://picsum.photos/id/237/1080/720'
+      },
+      {
+        id: '8',
+        name: 'Rick roll parka',
+        price: 299,
+        img: 'https://picsum.photos/id/237/1080/720'
+      }
+    ]
   },
   mutations: {
   },
