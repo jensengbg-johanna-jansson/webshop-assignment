be0daf5d0ac214d8ace14d19c1049aaeb4ab5c15
Johanna Jansson <joja92@hotmail.com>
2020-09-07 10:04:49 +0200

    Added test

--- src/tests/unit/cartButton.spec.js
+++ src/tests/unit/cartButton.spec.js
@@ -6,13 +6,30 @@ const localVue = createLocalVue();
 localVue.use(Vuex);
 
 describe('Header cart button tests', () => { 
-    it('should display the number of computed property "numberOfItemsInCart"', () => {
+    it('should display the number returned by "numberOfItemsInCart"', () => {
         const expected = '1';
         
         let store = new Vuex.Store({});
         let wrapper = shallowMount(cartButton, {
             computed: {
                 numberOfItemsInCart: () => { return 1 }
+              },
+              store,
+              localVue
+        });
+        let cartButtonNumber = wrapper.find('.cart-button-number');
+
+        let actual = cartButtonNumber.text();
+
+        expect(actual).toBe(expected);
+    })
+    it('shoudl display "0" if "numberOfItemsInCart" is not a number', () => {
+        const expected = '0';
+        
+        let store = new Vuex.Store({});
+        let wrapper = shallowMount(cartButton, {
+            computed: {
+                numberOfItemsInCart: () => { return undefined }
               },
               store,
               localVue
