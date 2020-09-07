6f191f465cea128a9b15cb8fcacc38eb59511313
Albin Sander <albinsander17@gmail.com>
2020-09-07 15:27:57 +0200

    Fixed product css

--- src/components/product.vue
+++ src/components/product.vue
@@ -1,6 +1,6 @@
 <template>
     <article class="product">
-        <img src="https://images.unsplash.com/photo-1512436991641-6745cdb1723f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80">
+        <img class="productImage" src="https://images.unsplash.com/photo-1512436991641-6745cdb1723f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80">
         <h2 class="productName">T-Shirt</h2>
         <h2 class="price">Price</h2>
 
@@ -19,6 +19,27 @@ export default {
 .product {
     display: grid;
     grid-template-columns: repeat(3,1fr);
+    
+
 }
+
+.productImage {
+    width: 12rem;
+    height: 10rem;
+}
+
+.productName {
+    display: flex;
+    align-self: center;
+    justify-self: center;
+}
+
+.price {
+    display: flex;
+    align-self: center;
+    justify-self: center;
+}
+
+
 
 </style>
