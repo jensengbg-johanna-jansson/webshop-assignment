a5f3a15b03fa7efd77089c99cbfe9ddaf290b635
jensen-ali-suliman <alxso@LAPTOP-92UALMD9.(none)>
2020-09-09 10:31:58 +0200

    Update delete.spec.js

--- tests/unit/delete.spec.js
+++ tests/unit/delete.spec.js
@@ -1,18 +1,35 @@
+import Vuex from 'vuex'
 import { shallowMount, createLocalVue } from '@vue/test-utils'
 import ProductList from '@/components/ProductList.vue'
 
+const localVue = createLocalVue()
+localVue.use(Vuex)
+
+const mockProductData = {
+  name: 'A mock T-shirt',
+  price: 299,
+  size: 'M',
+  img: 'https://picsum.photos/id/237/200/300',
+}
+
 describe('Delete product', () => {
   it('Should delete the clicked item', async () => {
-    const wrapper = shallowMount(ProductList)
+    const expected = 1
+    let store = new Vuex.Store({})
+
+    const wrapper = shallowMount(ProductList, {
+      propsData: {
+        cartItem: mockProductData,
+      },
+      store,
+      localVue,
+    })
+
     const deleteBtn = wrapper.find('.deleteBtn')
     await deleteBtn.trigger('click')
+
     const productToDelete = wrapper.vm.productToDelete
-
-    const cartItems = wrapper.vm.cartItems
-    console.log(cartItems.indexOf(productToDelete))
-
-    const expected = -1
-    const actuall = cartItems.indexOf(productToDelete)
+    const actuall = productToDelete.length
     expect(expected).toBe(actuall)
   })
 })
