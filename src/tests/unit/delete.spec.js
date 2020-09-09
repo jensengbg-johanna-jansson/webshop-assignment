00c1952a1960c9b60874cd91b38c291205847780
jensen-ali-suliman <alxso@LAPTOP-92UALMD9.(none)>
2020-09-09 09:09:33 +0200

    Update delete.spec.js

--- src/tests/unit/delete.spec.js
+++ src/tests/unit/delete.spec.js
@@ -1,5 +1,5 @@
 import Vuex from 'vuex'
-import { mount, createLocalVue } from '@vue/test-utils'
+import { shallowMount, createLocalVue } from '@vue/test-utils'
 import ProductList from '@/components/ProductList.vue'
 
 const localVue = createLocalVue()
@@ -8,44 +8,28 @@ localVue.use(Vuex)
 const mockProductData = {
   name: 'A mock T-shirt',
   price: 299,
+  size: 'M',
   img: 'https://picsum.photos/id/237/200/300',
 }
 
 describe('Delete product', () => {
   it('Should delete the clicked item', async () => {
-    const expected = -1
+    const expected = 1
     let store = new Vuex.Store({})
 
-    const wrapper = mount(ProductList, {
+    const wrapper = shallowMount(ProductList, {
       propsData: {
-        item: mockProductData,
+        cartItem: mockProductData,
       },
       store,
       localVue,
     })
 
     const deleteBtn = wrapper.find('.deleteBtn')
-    console.log(deleteBtn)
-    // await deleteBtn.trigger('click')
-    const productToDelete = wrapper.vm.productToDelete
-    console.log(productToDelete)
+    await deleteBtn.trigger('click')
 
-    const actuall = -1
+    const productToDelete = wrapper.vm.productToDelete
+    const actuall = productToDelete.length
     expect(expected).toBe(actuall)
   })
 })
-
-/* 
-    
-    const wrapper = shallowMount(ProductList)
-    const deleteBtn = wrapper.find('.deleteBtn')
-    await deleteBtn.trigger('click')
-    const productToDelete = wrapper.vm.productToDelete
-
-    const cartItems = wrapper.vm.cartItems
-    console.log(cartItems.indexOf(productToDelete))
-
-    const expected = -1
-    const actuall = cartItems.indexOf(productToDelete)
-    expect(expected).toBe(actuall)
-    */
